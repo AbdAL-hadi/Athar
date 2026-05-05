196876743889dffee77a3387e3192155b3011694
yazee <yazeed.k2016@gmail.com>
2026-05-05 20:45:09 +0200

    .

--- routes/assetRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getImageAsset } from '../controllers/assetController.js';
-
-const router = express.Router();
-
-router.get('/:id', getImageAsset);
-router.get('/:id/:fileName', getImageAsset);
-
-export default router;
